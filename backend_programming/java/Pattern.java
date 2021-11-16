f55dbf1a227be88c38d372b2daf8799fd7f68199
Jessemy <86029626+jeyla380@users.noreply.github.com>
2021-11-16 11:58:56 -0700

    Add files via upload

---
+++ backend_programming/java/Pattern.java
@@ -0,0 +1,13 @@
+package jGrasp;
+
+public class Pattern {
+
+    public static void main(String[] args){
+
+        System.out.println("--*--");
+        System.out.println("-*-*-");
+        System.out.println("*---*");
+        System.out.println("-*-*-");
+        System.out.println("--*--");
+    }
+}
