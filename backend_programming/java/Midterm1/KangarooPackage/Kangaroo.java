8539b6af344a1b6702177e168b08e25b9bba996a
Jessemy <86029626+jeyla380@users.noreply.github.com>
2021-11-16 12:04:05 -0700

    Add files via upload

---
+++ backend_programming/java/Midterm1/KangarooPackage/Kangaroo.java
@@ -0,0 +1,27 @@
+package Midterm1.KangarooPackage;
+
+public class Kangaroo {
+
+    private int weight;
+
+
+    public void Kangaroo(int w){
+
+        weight = w;
+    }
+
+    public int getWeight(){
+
+        return weight;
+    }
+
+    public void setWeight(int w){
+
+        weight = w;
+    }
+
+    public void jump(){
+
+        System.out.println("jumping");
+    }
+}
