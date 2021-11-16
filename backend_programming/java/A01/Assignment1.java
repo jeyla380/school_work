984d1da389ebb0404cf0651229668034fdc4932e
Jessemy <86029626+jeyla380@users.noreply.github.com>
2021-11-16 12:19:33 -0700

    Create Assignment1.java

---
+++ backend_programming/java/A01/Assignment1.java
@@ -0,0 +1,37 @@
+/************************
+ * Author: Jessemy Lake
+ * Date: May 24, 2019
+ * Assignment: A01 - Print
+ ***********************/
+
+
+
+
+package A01;
+
+public class Assignment1 {
+
+    public static void main(String[] args){
+
+        System.out.println("Hi, I'm Jessemy. This is assignment A01.");
+        System.out.println();
+        System.out.println();
+
+        System.out.println("What I hope to get out of this class: ");
+        System.out.println();
+
+        System.out.println("Learning the basics of programming in Java, and hopefully get ideas of different ");
+        System.out.println("programming languages as well.");
+
+        System.out.println();
+        System.out.println();
+
+        System.out.println("What I am willing to put into the class: ");
+        System.out.println();
+
+        System.out.println("Complete the quizzes before class, complete the assignments before the due date, ");
+        System.out.println("complete the labs (hopefully in class), and show up to class. Use extra resources ");
+        System.out.println("such as the teacher, other students, tutors, etc.");
+
+    }
+}
