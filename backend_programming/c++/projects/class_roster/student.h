123755ebd095db350ff897bfa5de5c4186db27a8
Jessemy <86029626+jeyla380@users.noreply.github.com>
2022-10-29 21:02:54 -0500

    Update student.h

--- backend_programming/c++/projects/class_roster/student.h
+++ backend_programming/c++/projects/class_roster/student.h
@@ -8,7 +8,7 @@
 class Student
 {
 public:
-	Student(std::string studentId, std::string firstName, std::string lastName, std::string email, int age, int day1, int day2, int day3, DegreeProgram degreeProgram); //constructor
+	Student(std::string studentId, std::string firstName, std::string lastName, std::string email, int age, int days[3], DegreeProgram degreeProgram); //constructor
 	Student(); //default constructor
 
 	void SetStudentId(std::string studentId);
@@ -26,13 +26,9 @@ public:
 	void SetAge(int age);
 	int getAge() const;
 
-	void SetCourseDays1(int day1);
+	void SetCourseDays(int days[3]);
 	int getCourseDays1() const;
-
-	void SetCourseDays2(int day2);
 	int getCourseDays2() const;
-
-	void SetCourseDays3(int day3);
 	int getCourseDays3() const;
 
 	void SetDegreeProgram(DegreeProgram degreeProgram);
@@ -46,9 +42,10 @@ private:
 	std::string lastName;
 	std::string email;
 	int age;
-	int day1;
-	int day2;
-	int day3;
+	int days[3];
+	//int day1;
+	//int day2;
+	//int day3;
 	DegreeProgram degreeProgram;
 };
 
