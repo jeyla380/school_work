1e9d99ab266ba19b46411e0358cf49900a433891
Jessemy <86029626+jeyla380@users.noreply.github.com>
2023-08-28 13:00:07 -0500

    Update smallestNumber.java

--- backend_programming/java/WGU_Labs/smallestNumber.java
+++ backend_programming/java/WGU_Labs/smallestNumber.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class LabProgram {
    public static void main(String[] args) {
-      /* Type your code here. */
+
       Scanner scnr = new Scanner(System.in);
       
       int numOne = scnr.nextInt();
